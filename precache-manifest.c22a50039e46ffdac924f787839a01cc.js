41a4481466516d22918ff4ac28a69afc785ab2ff
wafiseptya <septya.wafi@gmail.com>
2019-04-11 08:27:49 +0700

    Updates

---
+++ precache-manifest.c22a50039e46ffdac924f787839a01cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/theme-changer-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4724cd428c1b670bde60",
+    "url": "/theme-changer-react/static/js/runtime~main.19d32408.js"
+  },
+  {
+    "revision": "b3f5bae6b10024234b70",
+    "url": "/theme-changer-react/static/js/main.70324377.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/theme-changer-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b3f5bae6b10024234b70",
+    "url": "/theme-changer-react/static/css/main.41f8ed36.chunk.css"
+  },
+  {
+    "revision": "a8bfa2889ee63e7e65b5de69a85014ab",
+    "url": "/theme-changer-react/index.html"
+  }
+];
